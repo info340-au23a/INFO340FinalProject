c9e176906ba960511f7dd37b3ffa4b3ab9f86ec4
Saltyflounder <yiyiw2@uw.edu>
2024-03-12 17:15:48 -0700

    update education design

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,10 +3,8 @@ import { Link } from 'react-router-dom';
 import FungusGenerator from './FungusGenerator';  // Import FungusGenerator component
 
 const Education = () => {
-
   return (
     <div>
-      <h2>Education</h2>
       <FungusGenerator />
       <Link to="/">Back to Home</Link>
     </div>
